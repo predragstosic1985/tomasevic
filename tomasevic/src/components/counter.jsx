e047f86ad822f50b1e044f50145b1004a70cc4e2
predragstosic1985 <predragstosic1985@gmail.com>
2024-05-11 22:19:28 +0200

    Counter days

--- tomasevic/src/components/counter.jsx
+++ tomasevic/src/components/counter.jsx
@@ -2,16 +2,34 @@ import React from "react";
 import { CountdownCircleTimer } from 'react-countdown-circle-timer'
 
 export const Counter = (props) => {
-  // Duration for 5 days in seconds
-  const duration = 5 * 24 * 60 * 60;
-
-  // Function to format remaining time
-  const formatTime = (time) => {
-    const days = Math.floor(time / (3600 * 24));
-    const hours = Math.floor((time % (3600 * 24)) / 3600);
-    const minutes = Math.floor((time % 3600) / 60);
-    return `${days}d ${hours}h ${minutes}m`;
+  const minuteSeconds = 60;
+  const hourSeconds = 3600;
+  const daySeconds = 86400;
+  const timerProps = {
+    isPlaying: true,
+    size: 120,
+    strokeWidth: 6
   };
+
+  const renderTime = (dimension, time) => {
+    return (
+      <div className="time-wrapper">
+        <div className="time">{time}</div>
+        <div>{dimension}</div>
+      </div>
+    );
+  };
+
+  const getTimeSeconds = (time) => (minuteSeconds - time) | 0;
+  const getTimeMinutes = (time) => ((time % hourSeconds) / minuteSeconds) | 0;
+  const getTimeHours = (time) => ((time % daySeconds) / hourSeconds) | 0;
+  const getTimeDays = (time) => (time / daySeconds) | 0;
+  const stratTime = Date.now() / 1000; // use UNIX timestamp in seconds
+  const endTime = stratTime + (5 * daySeconds); // Countdown for 5 days
+
+  const remainingTime = endTime - stratTime;
+  const days = Math.ceil(remainingTime / daySeconds);
+  const daysDuration = days * daySeconds;
   return (
     <div id="about">
       <div className="container">
@@ -24,15 +42,68 @@ export const Counter = (props) => {
             <div className="about-text">
               <h2>TOMAŠEVIĆ</h2>
               <p>
-                <CountdownCircleTimer
-                  isPlaying
-                  duration={duration}
-                  colors={[["#004777", 0.33], ["#F7B801", 0.33], ["#A30000"]]}
-                  onComplete={() => console.log('Timer completed')}
-                >
-                  {({ remainingTime }) => formatTime(remainingTime)}
-                </CountdownCircleTimer>
+                Nasa web stranica se trenutno azurira i bice aktivna za
+                <div className="countdownCircleTimer">
+                  <CountdownCircleTimer
+                    {...timerProps}
+                    colors="#7E2E84"
+                    duration={daysDuration}
+                    initialRemainingTime={remainingTime}
+                  >
+                    {({ elapsedTime, color }) => (
+                      <span style={{ color }}>
+                        {renderTime("dana", getTimeDays(daysDuration - elapsedTime))}
+                      </span>
+                    )}
+                  </CountdownCircleTimer>
+                  <CountdownCircleTimer
+                    {...timerProps}
+                    colors="#D14081"
+                    duration={daySeconds}
+                    initialRemainingTime={remainingTime % daySeconds}
+                    onComplete={(totalElapsedTime) => ({
+                      shouldRepeat: remainingTime - totalElapsedTime > hourSeconds
+                    })}
+                  >
+                    {({ elapsedTime, color }) => (
+                      <span style={{ color }}>
+                        {renderTime("sati", getTimeHours(daySeconds - elapsedTime))}
+                      </span>
+                    )}
+                  </CountdownCircleTimer>
+                  <CountdownCircleTimer
+                    {...timerProps}
+                    colors="#EF798A"
+                    duration={hourSeconds}
+                    initialRemainingTime={remainingTime % hourSeconds}
+                    onComplete={(totalElapsedTime) => ({
+                      shouldRepeat: remainingTime - totalElapsedTime > minuteSeconds
+                    })}
+                  >
+                    {({ elapsedTime, color }) => (
+                      <span style={{ color }}>
+                        {renderTime("minuta", getTimeMinutes(hourSeconds - elapsedTime))}
+                      </span>
+                    )}
+                  </CountdownCircleTimer>
+                  <CountdownCircleTimer
+                    {...timerProps}
+                    colors="#218380"
+                    duration={minuteSeconds}
+                    initialRemainingTime={remainingTime % minuteSeconds}
+                    onComplete={(totalElapsedTime) => ({
+                      shouldRepeat: remainingTime - totalElapsedTime > 0
+                    })}
+                  >
+                    {({ elapsedTime, color }) => (
+                      <span style={{ color }}>
+                        {renderTime("sekundi", getTimeSeconds(elapsedTime))}
+                      </span>
+                    )}
+                  </CountdownCircleTimer>
+                </div>
               </p>
+              Hvala vam na strpljenju!
             </div>
           </div>
         </div>
